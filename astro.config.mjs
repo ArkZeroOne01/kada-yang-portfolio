c618322a14cdd08bceee1fd60e32b92ff905179a
ArkZeroOne <114424892+ArkZeroOne01@users.noreply.github.com>
2026-09-11 16:43:18 +0800

    Refactor astro.config.mjs for development settings
    Updated configuration to conditionally include keystatic integration in development mode and changed output to static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel";
+
+const isDevelopment = process.argv.includes("dev");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-
+  site: "https://kadayang.com",
+  output: "static",
+  integrations: [
+    react(),
+    markdoc(),
+    ...(isDevelopment ? [keystatic()] : []),
+  ],
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ["@keystatic/core", "@keystatic/astro"],
     },
   },
-
-  output: "server",
-
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
 });
